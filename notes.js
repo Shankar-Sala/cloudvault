bd96577f9e44fd18f8749488d17462f61a5e2652
Shankar-Sala <shankarsala9995@gmail.com>
2025-02-20 00:34:57 +0530

    ESLint

--- notes.js
+++ notes.js
@@ -1,5 +1,15 @@
 //? WebStorm IDE
 //* --> Setup & MetaData
 //? terminal type
-// --> npx create-next-app ./
-//--> npm run dev
+// 1.      npx create-next-app ./
+//2.      npm run dev
+//--> npm install --save-dev eslint-config-standard eslint-plugin-tailwindcss eslint-config-prettier prettier  
+
+// error while install the above packages 👆
+// solution : 
+// 3.    npm install --save-dev eslint-config-standard eslint-plugin-tailwindcss eslint-config-prettier prettier --legacy-peer-deps
+
+// 4.  npm install eslint@8.57.1 --save-dev
+// 5. npm i --save-dev tailwindcss-animate 
+
+// 6. install extension : -->  ES7+ React/Redux/React-Native snippets
