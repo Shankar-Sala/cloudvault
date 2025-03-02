a5a1caedb4ee4061d89dba740446e38d7764c60c
Shankar-Sala <shankarsala9995@gmail.com>
2025-03-02 21:11:33 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="h1">
+        CloudVault - The only storage solution you need.
+      </h1>
+    </div>
+  );
+}
+ 
---
+++ notes.js
@@ -0,0 +1,56 @@
+//? WebStorm IDE
+//* --> Setup & MetaData
+//? terminal type
+// 1.      npx create-next-app ./
+// 2.       npm run dev
+
+//-->      npm install --save-dev eslint-config-standard eslint-plugin-tailwindcss eslint-config-prettier prettier
+// error while install the above packages 👆
+// solution :
+// 3.        npm install --save-dev eslint-config-standard eslint-plugin-tailwindcss eslint-config-prettier prettier --legacy-peer-deps
+
+// 4.        npm install eslint@8.57.1 --save-dev
+// 5.        npm i --save-dev tailwindcss-animate
+
+// 6. install extension : -->  ES7+ React/Redux/React-Native snippets
+// url : http://localhost:3000/sign-up
+// link: https://ui.shadcn.com/
+
+
+
+// 7.        npx shadcn@latest init
+                  // New York
+                  // Neutral
+                  // use --force
+
+//8.         npx shadcn@latest add form
+                  // Use --force
+
+//9.             npx shadcn@latest add input
+
+// ----------------------------------------------------------------------------
+
+// Setup AppWrite & Create Account
+
+//10.      npm install node-appwrite --save
+
+// note: Create file   .env.local
+//  this file not track by github
+// and paste this 👇🏻
+
+// NEXT_PUBLIC_APPWRITE_ENDPOINT="https://cloud.appwrite.io/v1"
+// NEXT_PUBLIC_APPWRITE_PROJECT="67b8348b00071ebc387e"
+// NEXT_PUBLIC_APPWRITE_DATABASE="67b83614002853a30c86"
+// NEXT_PUBLIC_APPWRITE_USERS_COLLECTION="67b8366d000dffd9dc06"
+// NEXT_PUBLIC_APPWRITE_FILES_COLLECTION="67b838db002696a7eab9"
+// NEXT_PUBLIC_APPWRITE_BUCKET="67b83bb300071d53141e"
+// NEXT_APPWRITE_SECRET="standard_b7107a80ef5ad45b5da47b43eaa589a35e7d7946c6ba7d4a180996af2294c05f13ffe80e7c0f3a9e0423b180f05daa4b08dcbd152f34f52feca6cb2bcda99dcad6e7f0a40ca8be1bf395805e217c540a7c98791e5314a560c868044818ea6f6f9c16d95ad82857b6a9bb0a9ebe0887d2e8acdc5a2250554add95c2b4ea273818"
+
+// ------------------------------------------
+
+// 11.          npm install node-appwrite
+// 12.          npx shadcn@latest add alert-dialog
+// use --force
+
+//13.           npx shadcn@latest add input-otp
+//              Use --force
